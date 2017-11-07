d9159e904ea60ce076e3e7df2bd85eb25bce2235
Stewart ilondanga <stilondanga@gmail.com>
2017-11-07 16:04:52 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,6 @@
   $('#form').on('submit', function(event) {
 
 
-
-
-
     //Set the text-output span to the value of the first input
     var input = $("#input").val();
     $('#text-output').text("You typed: " + input);
@@ -13,3 +10,19 @@
     //Prevent the page from reloading
     event.preventDefault();
   });
+
+function startTime(){
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if(i<10){i ="0" + i}; //add zero in front of numbers<10
+  return i;
+}
