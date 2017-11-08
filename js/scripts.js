c1ba96d0f6871fb5dfe6115e66db0c2b1cf31412
samuel luka <samluka99@gmail.com>
2017-11-08 09:43:43 +0300

    Added a Footer Section

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,25 @@ $('#form').on('submit', function(event) {
 
 
    var input = $("#input").val();
-   $('#text-output').text("You typed: " + input);
+   $('#text-output').text(" " + input);
 
 
    event.preventDefault();
  });
+
+
+ function temperatureConverter(valNum) {
+   valNum = parseFloat(valNum);
+   document.getElementById("outputCelcius").innerHTML=valNum-273.15;
+ }
+
+ function myFunction() {
+     // Get the snackbar DIV
+     var x = document.getElementById("snackbar")
+
+     // Add the "show" class to DIV
+     x.className = "show";
+
+     // After 3 seconds, remove the show class from DIV
+     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+ }
