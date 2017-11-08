cb622cf3a0e1448878d386177c43c7ebe7739f74
samuel luka <samluka99@gmail.com>
2017-11-08 09:55:24 +0300

    added a snackbar

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,14 @@ $('#form').on('submit', function(event) {
     valNum = parseFloat(valNum);
     document.getElementById("outputCelcius").innerHTML=valNum-273.15;
   }
+
+  function myFunction() {
+       // Get the snackbar DIV
+       var x = document.getElementById("snackbar")
+
+       // Add the "show" class to DIV
+       x.className = "show";
+
+       // After 3 seconds, remove the show class from DIV
+       setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+   }
