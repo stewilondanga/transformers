68782687d4c987ae7454c19f558ecf5aa5457ef3
Stewart ilondanga <stilondanga@gmail.com>
2017-11-08 13:07:42 +0300

    addindex.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 //Attach an event fro when the user submits the form
 
-  $('#form').on('submit', function(event) {
+$('#form').on('submit', function(event) {
 
 
-    //Set the text-output span to the value of the first input
-    var input = $("#input").val();
-    $('#text-output').text("You typed: " + input);
+  //Set the text-output span to the value of the first input
+  var input = $("#input").val();
+  $('#text-output').text("You typed: " + input);
 
-    //Prevent the page from reloading
-    event.preventDefault();
-  });
+  //Prevent the page from reloading
+  event.preventDefault();
+});
 
-function startTime(){
+function startTime() {
   var today = new Date();
   var h = today.getHours();
   var m = today.getMinutes();
@@ -19,10 +19,20 @@ function startTime(){
   m = checkTime(m);
   s = checkTime(s);
   document.getElementById('txt').innerHTML =
-  h + ":" + m + ":" + s;
+    h + ":" + m + ":" + s;
   var t = setTimeout(startTime, 500);
 }
+
 function checkTime(i) {
-  if(i<10){i ="0" + i}; //add zero in front of numbers<10
+  if (i < 10) {
+    i = "0" + i
+  }; //add zero in front of numbers<10
   return i;
 }
+
+$(document).ready(function(){
+  $("#button").click(function(){
+    //action
+    alert("Lets communicate!");
+  });
+});
